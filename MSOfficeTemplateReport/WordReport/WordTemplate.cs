4cc71d91fdf04329e0b14e7e7e5471e7b7b7d309
Александр Мокринский <mokrynskiy@gmail.com>
2024-04-25 11:35:09 +0700

    fix

---
+++ MSOfficeTemplateReport/WordReport/WordTemplate.cs
@@ -0,0 +1,273 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using DocumentFormat.OpenXml;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using OpenXmlPowerTools;
+using Text = DocumentFormat.OpenXml.Wordprocessing.Text;
+using Table = DocumentFormat.OpenXml.Wordprocessing.Table;
+using Break = DocumentFormat.OpenXml.Wordprocessing.Break;
+using TableRow = DocumentFormat.OpenXml.Wordprocessing.TableRow;
+
+namespace WordTemplateReport.WordReport
+{
+    public sealed class WordTemplate
+    {
+        private string _path;
+        private Dictionary<string, object> _variables;
+        private MemoryStream _ms;
+        private WordprocessingDocument _document;
+        private readonly Regex _regex = new Regex("\\{\\{.*?\\}\\}");
+        private readonly Regex _tagRegex = new Regex("[\\{]{2}[a-zA-Z.]+[\\}]{2}");
+        private readonly Regex _itemRegex = new Regex("Item");
+
+        public WordTemplate(string path)
+        {
+            _path = path;
+            _variables = new Dictionary<string, object>();
+        }
+
+        public void AddVariable(string name, object data) => this._variables.Add(name, data);
+
+        public void Generate()
+        {
+            try
+            {
+                byte[] buffer = File.ReadAllBytes(this._path);
+                MemoryStream memoryStream = new MemoryStream();
+                memoryStream.Write(buffer, 0, buffer.Length);
+                _document = WordprocessingDocument.Open((Stream)memoryStream, true);
+                CleanDoc();
+                FillHeader();
+                FillFooter();
+                FillBody();
+            }
+            catch (Exception ex)
+            {
+                _document.Dispose();
+                throw ex;
+            }
+        }
+        private void FillBody()
+        {
+            var body = _document.MainDocumentPart.Document.Body;
+            if (body == null) return;
+            var text = body.Descendants<Text>();
+            var tables = body.Descendants<TableProperties>();
+            if (text != null) FillText(text);
+            if (tables != null) FillTables(tables);
+        }
+        private void FillFooter()
+        {
+            var footer = _document.MainDocumentPart.FooterParts.FirstOrDefault();
+            if (footer == null) return;
+            var text = footer.Footer.Descendants<Text>();
+            var tables = footer.Footer.Descendants<TableProperties>();
+            if (text != null) FillText(text);
+            if (tables != null) FillTables(tables);
+        }
+        private void FillHeader()
+        {
+            var header = _document.MainDocumentPart.HeaderParts.FirstOrDefault();
+            if (header == null) return;
+            var text = header.Header.Descendants<Text>();
+            var tables = header.Header.Descendants<TableProperties>();
+            if(text != null) FillText(text);
+            if (tables != null) FillTables(tables);
+        }
+        private void FillText (IEnumerable<Text> text)
+        {
+            foreach (var txt in text)
+            {
+                if (_regex.IsMatch(txt.Text))
+                {
+                    foreach (object match in this._tagRegex.Matches(txt.Text))
+                    {
+                        string[] strArray = match.ToString().Replace("{", "").Replace("}", "").Split('.');
+                        if (strArray.Length == 2)
+                        {
+                            string key = strArray[0];
+                            if (!(key == "Item"))
+                            {
+                                string name = strArray[1];
+                                object obj = _variables.Where<KeyValuePair<string, object>>((Func<KeyValuePair<string, object>, bool>)(v => v.Key == key)).Select<KeyValuePair<string, object>, object>((Func<KeyValuePair<string, object>, object>)(v => v.Value)).FirstOrDefault<object>();
+                                if (obj != null)
+                                {
+                                    string str = obj.GetType().GetProperty(name)?.GetValue(obj)?.ToString();
+                                    if (str != null)
+                                    {
+                                        if (str.Contains("rtf1"))
+                                        {
+                                            string id = name + (new Random(1000000).Next());
+                                            using (MemoryStream sourceStream = new MemoryStream(Encoding.ASCII.GetBytes(str)))
+                                                _document.MainDocumentPart.AddAlternativeFormatImportPart(AlternativeFormatImportPartType.Rtf, id).FeedData((Stream)sourceStream);
+                                            txt.InsertAfterSelf<AltChunk>(new AltChunk()
+                                            {
+                                                Id = (StringValue)id
+                                            });
+                                            txt.Text = txt.Text.Replace(match.ToString(), "");
+                                        }
+                                        else if (str.Contains("<HTML"))
+                                        {
+                                            string id = name + (new Random(1000000).Next());
+                                            using (MemoryStream sourceStream = new MemoryStream(Encoding.UTF8.GetBytes(str)))
+                                                _document.MainDocumentPart.AddAlternativeFormatImportPart(AlternativeFormatImportPartType.Html, id).FeedData((Stream)sourceStream);
+                                            txt.InsertAfterSelf<AltChunk>(new AltChunk()
+                                            {
+                                                Id = (StringValue)id
+                                            });
+                                            txt.Text = txt.Text.Replace(match.ToString(), "");
+                                        }
+                                        else
+                                            txt.Text = txt.Text.Replace(match.ToString(), str);
+                                    }
+                                    else
+                                        txt.Text = txt.Text.Replace(match.ToString(), "[" + match.ToString() + " - значение было null]");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        private void FillTables(IEnumerable<TableProperties> tables)
+        {
+            foreach (TableProperties table in tables)
+            {
+                string tableCaption = table.TableCaption?.Val?.ToString();
+                if (tableCaption != null)
+                {
+                    IEnumerable<KeyValuePair<string, object>> variable = _variables.Where<KeyValuePair<string, object>>((Func<KeyValuePair<string, object>, bool>)(v => v.Key == tableCaption));
+                    if (variable.Any<KeyValuePair<string, object>>())
+                    {
+                        var values = variable.FirstOrDefault<KeyValuePair<string, object>>().Value;
+                        values.GetType();
+                        if (values != null)
+                        {
+                            IList list = (IList)values;
+                            Table parent = (Table)table.Parent;
+                            TableRow tableRow = null;
+                            foreach (var row in parent.Descendants<TableRow>())
+                            {
+                                IEnumerable<Text> textList = row.Descendants<Text>().Where((Func<Text, bool>)(t => _regex.IsMatch(t.Text)));
+                                if (textList.Any() && this._itemRegex.IsMatch(textList.FirstOrDefault().Text))
+                                {
+                                    tableRow = row;
+                                    break;
+                                }
+                            }
+                            if (tableRow != null)
+                            {
+                                int valueIndex = list.Count - 1;
+                                for (int i = 1; i < list.Count; ++i)
+                                {
+                                    var clonedRow = (TableRow)tableRow.CloneNode(true);
+                                    var value = list[valueIndex];
+                                    tableRow.InsertAfterSelf(clonedRow);
+                                    foreach (var txt in clonedRow.Descendants<Text>())
+                                    {
+                                        foreach (object match in _tagRegex.Matches(txt.Text))
+                                        {
+                                            string[] tagValue = match.ToString().Replace("{", "").Replace("}", "").Split('.');
+                                            if (((IEnumerable<string>)tagValue).Count<string>() == 2 && tagValue[0] == "Item")
+                                            {
+                                                string name = tagValue[1];
+                                                string str = value.GetType().GetProperty(name)?.GetValue(value)?.ToString();
+                                                if (str != null)
+                                                {
+                                                    if (str.Contains("rtf1"))
+                                                    {
+                                                        string id = name + i.ToString();
+                                                        using (MemoryStream sourceStream = new MemoryStream(Encoding.ASCII.GetBytes(str)))
+                                                            _document.MainDocumentPart.AddAlternativeFormatImportPart(AlternativeFormatImportPartType.Rtf, id).FeedData((Stream)sourceStream);
+                                                        txt.InsertAfterSelf<AltChunk>(new AltChunk()
+                                                        {
+                                                            Id = (StringValue)id
+                                                        });
+                                                        txt.Text = txt.Text.Replace(match.ToString(), "");
+                                                    }
+                                                    else if (str.Contains("<HTML"))
+                                                    {
+                                                        string id = name + i.ToString();
+                                                        using (MemoryStream sourceStream = new MemoryStream(Encoding.UTF8.GetBytes(str)))
+                                                            _document.MainDocumentPart.AddAlternativeFormatImportPart(AlternativeFormatImportPartType.Html, id).FeedData((Stream)sourceStream);
+                                                        AltChunk newElement2 = new AltChunk();
+                                                        newElement2.Id = (StringValue)id;
+                                                        txt.InsertAfterSelf<AltChunk>(newElement2);
+                                                        txt.Remove();
+                                                        IEnumerable<Break> b = newElement2.Parent.Descendants<Break>();
+                                                        Console.WriteLine(b.Count<Break>());
+                                                        if (b.Any<Break>())
+                                                            b.Last<Break>().Remove();
+                                                    }
+                                                    else
+                                                        txt.Text = txt.Text.Replace(match.ToString(), str);
+                                                }
+                                                else
+                                                    txt.Text = txt.Text.Replace(match.ToString(), "[" + match.ToString() + " - значение было null]");
+                                            }
+                                        }
+                                    }
+                                    --valueIndex;
+                                }
+                                foreach (var txt in tableRow.Descendants<Text>())
+                                {
+                                    var listIndex = list[valueIndex];
+                                    foreach (object match in this._tagRegex.Matches(txt.Text))
+                                    {
+                                        string[] source5 = match.ToString().Replace("{", "").Replace("}", "").Split('.');
+                                        if (((IEnumerable<string>)source5).Count<string>() == 2 && !(source5[0] != "Item"))
+                                        {
+                                            string name = source5[1];
+                                            string newValue = listIndex.GetType().GetProperty(name)?.GetValue(listIndex)?.ToString();
+                                            if (newValue != null)
+                                                txt.Text = txt.Text.Replace(match.ToString(), newValue);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        private void CleanDoc()
+        {
+            MarkupSimplifier.SimplifyMarkup(_document, new SimplifyMarkupSettings()
+            {
+                RemoveComments = true,
+                RemoveContentControls = true,
+                RemoveEndAndFootNotes = true,
+                RemoveFieldCodes = false,
+                RemoveLastRenderedPageBreak = true,
+                RemovePermissions = true,
+                RemoveProof = true,
+                RemoveRsidInfo = true,
+                RemoveSmartTags = true,
+                RemoveSoftHyphens = true,
+                ReplaceTabsWithSpaces = true
+            });
+            _document.CleanRun();
+        }
+        public string SaveAs(string outputFilePath)
+        {
+            try
+            {
+                OpenXmlPackage openXmlPackage = _document.Clone(outputFilePath);
+                _document.Dispose();
+                openXmlPackage.Dispose();
+                return outputFilePath;
+            }
+            catch (Exception ex)
+            {
+                _document.Dispose();
+                throw ex;
+            }
+        }
+    }
+}
---
+++ TestProject/Program.cs
@@ -0,0 +1,54 @@
+﻿using WordTemplateReport.WordReport;
+
+Header header = new()
+{
+    TitleA = "Заголовок1",
+    TitleB = "Заголовок2",
+    TitleC = @"Заголовок3",
+    Positions = new()
+    {
+        new() {PropA = "1.1", PropB = "1.2", PropC = "1.3"},
+        new() {PropA = "2.1", PropB = "2.2", PropC = "2.3"},
+        new() {PropA = "3.1", PropB = "3.2", PropC = "<HTML lang=\"ru\"><HEAD><meta charset=\"utf-8\"/></HEAD><BODY><DIV style=\"color:red\"> <b><u>2</u></b> наб., сер.2738, годен до 12.07.24, 21.31%; </DIV><DIV style=\"color:red\"> <b><u>16</u></b> наб., сер.2821, годен до 17.11.24, 56.28%; </DIV></BODY></HTML>"}
+    },
+    Users = new()
+    {
+        new(){Position = "Должность1", Name="Имя1", Role = 1},
+        new(){Position = "Должность2", Name="Имя2", Role = 1},
+        new(){Position = "Должность3", Name="Имя3", Role = 1},
+        new(){Position = "Должность4", Name="Имя4", Role = 1},
+        new(){Position = "Должность5", Name="Имя5", Role = 2},
+        new(){Position = "Должность6", Name="Имя6", Role = 2},
+    }
+};
+string resultFileName = "Result.docx";
+var template = new WordTemplate("TestTemplate.docx");
+template.AddVariable("Header", header);
+template.AddVariable("Prod", header.Positions);
+template.AddVariable("First", header.Users.Where(x => x.Role == 1).ToList());
+template.AddVariable("Second", header.Users.Where(x => x.Role == 2).ToList());
+template.Generate();
+template.SaveAs(resultFileName);
+
+
+class Header
+{
+    public string TitleA { get; set; }
+    public string TitleB { get; set; }
+    public string TitleC { get; set; }
+    public List<Positions> Positions { get; set; }
+    public List<Users> Users { get; set; }
+}
+
+class Positions
+{
+    public string PropA { get; set; }
+    public string PropB { get; set; }
+    public string PropC { get; set; }
+}
+class Users
+{
+    public string Position { get; set; }
+    public string Name { get; set; }
+    public int Role { get; set; }
+}
