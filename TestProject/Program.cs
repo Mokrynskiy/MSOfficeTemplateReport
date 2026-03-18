76532b87478ac65e17949d3f6ea6d9db65e76a44
Александр Мокринский <Mokrinskiy@vector-best.ru>
2026-03-18 16:29:10 +0700

    fix

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,18 +1,12 @@
 ﻿using MSOfficeTemplateReport;
 using TestProject;
 
-const string excelTemplatePath = "testexceltemplate.xlsx";
-
 const string wordTemplatePath = "testwordtemplate.docx";
-
-Dictionary<string, object> variables = new Dictionary<string, object>();
-
-variables.Add("Order", new Order());
 
 var template = Template.Create(wordTemplatePath);
 
 template.AddVariable("Order", new Order());
 
-var result = template.Generate("result");
+var result = template.Generate();
 
 result.SaveAs("result.xlsx");
